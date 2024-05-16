4830a1d43e4860f4997da21e2d88cdfc997eccd8
azishio <azishio3141@gmail.com>
2024-05-17 03:03:35 +0900

    ✨ lasファイルからVoxelTileの生成をサポート

--- src/tiler.rs
+++ src/tiler.rs
@@ -1,3 +1,6 @@
+use std::fmt::Debug;
+
+use coordinate_transformer::{pixel2ll, pixel_resolution};
 use vec_x::VecX;
 
 use crate::{PixelPointCloud, VoxelCollection, VoxelMesh};
@@ -10,12 +13,38 @@ pub struct VoxelTile {
 pub struct VoxelTiler {}
 
 impl VoxelTiler {
-    pub fn from_point_cloud(point_cloud: PixelPointCloud) -> Vec<VoxelTile> {
-        let tiled_points = point_cloud.split_by_tile();
+    #[cfg(feature = "las")]
+    pub fn from_jpr_las<T: std::io::Read + Seek + Send + Debug>(las: T, jpr_origin: JprOrigin, zoom_lv_list: Vec<ZoomLv>, rotate: bool) -> Vec<(ZoomLv, Vec<VoxelTile>)> {
+        let mut reader = Reader::new(BufReader::new(las)).unwrap();
 
 
-        tiled_points.into_iter().map(|(tile_idx, pixel_point_cloud)| {
-            let voxel_collection = VoxelCollection::from_pixel_point_cloud(pixel_point_cloud);
+        let zoom_lv_set = IndexSet::<ZoomLv, FxBuildHasher>::from_iter(zoom_lv_list);
+
+        zoom_lv_set.into_iter().map(|zoom_lv| {
+            let jpr_points = reader.points().map(|wrapped_points| {
+                let point = wrapped_points.unwrap();
+
+                // 時々Lasファイルでのxが平面直角座標系のyになっていることがあるので、rotate引数で対応
+                let (long, lat) = if !rotate { jpr2ll((point.y, point.x), jpr_origin) } else { jpr2ll((point.x, point.y), jpr_origin) };
+
+                let (x, y) = ll2pixel((long, lat), zoom_lv);
+
+                let pixel_resolution = pixel_resolution(lat, zoom_lv);
+
+                let z = (point.z / pixel_resolution) as u32;
+
+                let color = point.color.unwrap_or(Color::new(0, 0, 0));
+
+                let r = (color.red / u8::MAX as u16) as u8;
+                let g = (color.green / u8::MAX as u16) as u8;
+                let b = (color.blue / u8::MAX as u16) as u8;
+
+                (Coord::new([x, y, z]), RGB::new([r, g, b]))
+            }).collect::<Vec<Point<u32>>>();
+
+            let point_cloud = PixelPointCloud::new(jpr_points, zoom_lv);
+
+            let voxel_tile_list = Self::from_pixel_point_cloud(point_cloud);
 
             (zoom_lv, voxel_tile_list)
         }).collect::<Vec<_>>()
