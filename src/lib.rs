7c50d202df46dc8a1587175c2c3e183da2712391
azishio <azishio3141@gmail.com>
2024-05-19 08:18:51 +0900

    ✨ jamallocに対応
    これを適用することでパフォーマンスの大幅な向上が確認できた

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,17 @@
+#[cfg(not(target_env = "msvc"))]
+#[cfg(feature = "jamalloc")]
+use tikv_jemallocator::Jemalloc;
+
 pub use mesh::*;
 pub use params::*;
 pub use ply::*;
 pub use voxel::*;
 pub use voxelizer::*;
+
+#[cfg(not(target_env = "msvc"))]
+#[cfg(feature = "jamalloc")]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
 
 mod voxel;
 mod ply;
