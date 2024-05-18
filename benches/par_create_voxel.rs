61832859f47ea34ee9d1b90ef18cea9c5a8d9d9d
azishio <azishio3141@gmail.com>
2024-05-19 08:05:46 +0900

    📈 コンパイラの最適化を避ける

--- benches/par_create_voxel.rs
+++ benches/par_create_voxel.rs
@@ -3,6 +3,8 @@ use std::io::BufReader;
 
 use coordinate_transformer::{JprOrigin, ZoomLv};
 use criterion::{Criterion, criterion_group, criterion_main};
+#[cfg(feature = "rayon")]
+use rayon::prelude::*;
 
 use voxel_tiler_core::{PlyStructs, Voxelizer};
 use voxel_tiler_core::default_params::{Fit, Tile};
@@ -12,15 +14,15 @@ fn create_voxel() -> Vec<Vec<u8>> {
 
     let v = Voxelizer::<Fit>::voxelize_from_jpr_las(las, JprOrigin::One, ZoomLv::Lv17, false);
 
-    v.into_iter().map(|(_, voxel_mesh)| PlyStructs::from_voxel_mesh(voxel_mesh).to_ascii_ply_buf()).collect::<Vec<_>>()
+    v.into_par_iter().map(|(_, voxel_mesh)| PlyStructs::from_voxel_mesh(voxel_mesh).to_ascii_ply_buf()).collect::<Vec<_>>()
 }
 
 fn create_voxel_tile() -> Vec<Vec<u8>> {
     let las = BufReader::new(File::open("./examples/data-source/01JE2423.las").unwrap());
 
-    let v = Voxelizer::<Fit>::voxelize_from_jpr_las(las, JprOrigin::One, ZoomLv::Lv17, false);
+    let v = Voxelizer::<Tile>::voxelize_from_jpr_las(las, JprOrigin::One, ZoomLv::Lv17, false);
 
-    v.into_iter().map(|(_, voxel_mesh)| PlyStructs::from_voxel_mesh(voxel_mesh).to_ascii_ply_buf()).collect::<Vec<_>>()
+    v.into_par_iter().map(|(_, voxel_mesh)| PlyStructs::from_voxel_mesh(voxel_mesh).to_ascii_ply_buf()).collect::<Vec<_>>()
 }
 
 pub fn criterion_benchmark(c: &mut Criterion) {
@@ -33,7 +35,7 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("create_voxel");
     group.sample_size(10);
 
-    if cfg!(not(feature = "rayon")) {
+    if cfg!(feature = "rayon") {
         group.bench_function("fit", |b| b.iter(create_voxel));
         group.bench_function("tile", |b| b.iter(create_voxel_tile));
     }
