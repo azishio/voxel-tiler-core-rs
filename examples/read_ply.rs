839986c1900a2a63e88352fbaebfcfdd358831d7
azishio <azishio3141@gmail.com>
2024-05-18 17:14:25 +0900

    ✨ 例の追加

--- examples/read_ply.rs
+++ examples/read_ply.rs
@@ -17,6 +17,18 @@ fn main() {
 
     // from buffer
     let file: Vec<u8> = std::fs::read("examples/data-source/box.ply").unwrap();
-    let ply = PlyStructs::from_ply(file.as_slice());
+    let ply_by_buf = PlyStructs::from_ply(file.as_slice());
+    println!("{:?}", ply_by_buf);
+
+    // 対応していないプロパティは警告をプリントして無視されます。
+    // 警告をプリントさせたくない場合は、`print-warning`featureフラグをおろしてください。
+    // Unsupported properties will print a warning and be ignored.
+    // If you do not want warnings to be printed, please turn off the `print-warning` feature flag.
+    let file = File::open("examples/data-source/have_unsupported_properties_box.ply").unwrap();
+    let ply = PlyStructs::from_ply(file);
     println!("{:?}", ply);
+
+    // `box.ply`と`have_unsupported_properties_box.ply`において、対応するプロパティは同じです。
+    // In `box.ply` and `have_unsupported_properties_box.ply`, the corresponding properties are the same.
+    assert_eq!(ascii_ply.to_ascii_ply_buf(), ply.to_ascii_ply_buf());
 }
