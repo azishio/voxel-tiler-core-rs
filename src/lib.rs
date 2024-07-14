b9d76ff6ae315566b3b986ac7bdd9bd6a401dcd9
azishio <azishio3141@gmail.com>
2024-07-15 06:02:06 +0900

    📝 ドキュメンテーションコメントの追加

--- src/lib.rs
+++ src/lib.rs
@@ -15,17 +15,25 @@ pub use vec_x;
 #[global_allocator]
 static GLOBAL: Jemalloc = Jemalloc;
 
+/// 点群をボクセル化するためのモジュールです。
 pub mod voxelizer;
+/// 基本的な使用方法にあったデフォルトの設定を適用したボクセライザーを構築するためのモジュールです。
 pub mod build_voxelizer;
+/// 変換前の点群やボクセルデータを格納する構造体です。
+/// 実装によって高速/低速になる処理が違うため、目的によって使い分けてください。
 pub mod collection;
+/// ボクセルデータを表現するための座標値やRGB色などを表す構造体を定義しています。
 pub mod element;
+/// 国土地理院が公開する標高タイルを用いてボクセルデータを生成するためのモジュールです。
 #[cfg_attr(docsrs, doc(cfg(feature = "image")))]
 #[cfg(feature = "image")]
 pub mod giaj_terrain;
+/// glbファイルにメッシュを書き込むためのモジュールです。
 pub mod glb;
 #[cfg_attr(docsrs, doc(cfg(feature = "ply")))]
 #[cfg(feature = "ply")]
 pub mod ply;
+/// ボクセル化された点群にメッシュを貼るためのモジュール。
 pub mod mesh;
 
 /// lasファイルから点群を読むためのモジュールです。
